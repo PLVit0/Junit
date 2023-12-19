f8d941930b78e9a2f54d0202f5c61a55375487c6
PLVit0 <153430127+PLVit0@users.noreply.github.com>
2023-12-19 21:24:19 +0100

    Added ososba_it1

--- src/main/java/pl/kurs/osoba_it1/Main.java
+++ src/main/java/pl/kurs/osoba_it1/Main.java
@@ -7,25 +7,6 @@ import java.util.function.Predicate;
 public class Main {
     public static void main(String[] args) throws NoOldestException, NoWomanException {
 
-        // Stwórz klase osoba, osoba ma imie, miasto, wiek i napisz ponizsze metody // DONE
-
-        // policz ile jest osob starszych niz podany wiek // DONE
-
-        //znajdz najstarsza osobe z listy, jesli nie ma to rzuc wyjatkiem NoOldestException // DONE
-
-        //znajdz najstarsza kobiete, jesli takiej ni ema to rzuc wyjatkiem NoWomenException / DONE
-
-        //zwroc liste mezcyzzn // DONE
-
-        //oblicz sredni wiek mezczyzn //DONE
-
-        // * - metode ktora agreguje dwie metody powyzej (tzn: jako drugi parametr
-        // przyjmuje funkcje ktora okresla plec) // ???
-
-        //  metode ktora zwroci wszystkie rozne miasta z listy osob(rozne tzn: bez powtorzen) // DONE
-
-        // metode ktora znajdze miasto w ktorym zyje najwiecej ludzi // DONE
-
         Osoba osoba1 = new Osoba("Tomek", "Warszawa", 30);
         Osoba osoba2 = new Osoba("Alan", "Lodz", 40);
         Osoba osoba3 = new Osoba("Jan", "Torun", 20);
